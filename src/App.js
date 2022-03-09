460d85456a334889b81b6e9771eda11d39dd7c88
nitink-prog <80429955+nitink-prog@users.noreply.github.com>
2022-03-09 13:48:02 -0500

    preparing to be able to store a 'matched' card prop

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import SingleCard from "./components/SingleCard";
 import "./App.css";
 
 const cardImages = [
-  { src: "/img/helmet-1.png " },
-  { src: "/img/potion-1.png " },
-  { src: "/img/ring-1.png " },
-  { src: "/img/scroll-1.png " },
-  { src: "/img/shield-1.png " },
-  { src: "/img/sword-1.png " },
+  { src: "/img/helmet-1.png", matched: false },
+  { src: "/img/potion-1.png", matched: false },
+  { src: "/img/ring-1.png", matched: false },
+  { src: "/img/scroll-1.png", matched: false },
+  { src: "/img/shield-1.png", matched: false },
+  { src: "/img/sword-1.png", matched: false },
 ];
 
 function App() {
@@ -26,24 +26,36 @@ function App() {
     setTurns(0);
   };
 
-  const resetAndTurns = () => {
+  const resetTurns = () => {
     setChoiceOne(null);
     setChoiceTwo(null);
     setTurns((prevTurns) => prevTurns + 1);
   };
 
-  const compare = (cOne, cTwo) => {
-    cOne.src === cTwo.src ? console.log("match") : console.log("no");
-  };
   const handleChoice = (card) => {
     choiceOne ? setChoiceTwo(card) : setChoiceOne(card);
-    console.log(choiceOne, choiceTwo);
-
-    if (choiceOne && choiceTwo) {
-      compare(choiceOne, choiceTwo);
-      resetAndTurns();
-    }
   };
+
+  useEffect(() => {
+    if (choiceOne && choiceTwo) {
+      if (choiceOne.src === choiceTwo.src) {
+        setCards((prevCards) => {
+          return prevCards.map((card) => {
+            if (card.src === choiceOne.src) {
+              return { ...card, matched: true };
+            } else {
+              return card;
+            }
+          });
+        });
+      } else {
+        console.log("not");
+      }
+      resetTurns();
+    }
+  }, [choiceOne, choiceTwo]);
+
+  
 
   return (
     <div className="App">
